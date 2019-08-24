af7d536ec5d1f81e210f6ea03733085fefe7412d
Denis Paavilainen <denpa@denpa.pro>
2019-08-24 18:17:12 +0300

    Fixed SignedInt test.

--- tests/Types/SignedIntTest.php
+++ tests/Types/SignedIntTest.php
@@ -37,7 +37,7 @@ class SignedIntTest extends TestCase
 
         $signedint->expects($this->exactly(2))
             ->method('machineEndianness')
-            ->willReturnOnConsecutiveCalls(false, true);
+            ->willReturnOnConsecutiveCalls(SignedInt::LE, SignedInt::BE);
 
         $this->assertEquals($result1, $signedint->toHex());
         $this->assertEquals($result2, $signedint->toHex());
